a70acdad5612b08bf5227d88e38a186b44b8a0aa
Kristian Kjeldsen <jg@renegade.se>
2023-02-23 15:09:47 +0100

    added Navbar. Thanks Milad

---
+++ src/Navbar.jsx
@@ -0,0 +1,31 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+<h1>Welcome to my world</h1>
+    <div className="links">
+        <a href="/" style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px"
+        }}>Home</a>
+        <a href="/" style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px"
+        }}>Contact</a>
+        <a href="/" style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px"
+        }}>Portfolio</a>
+        <a href="/" style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px"
+        }}>About</a>
+    </div>
+    </nav>
+     );
+}
+ 
+export default Navbar;
