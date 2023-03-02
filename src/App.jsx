6ee71e8ecd937d1a168ab7b6f6a82eeeaf84343f
Kristian Kjeldsen <jg@renegade.se>
2023-03-02 10:10:30 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,7 @@ const MyButton = styled.button`
 
 
 function App() {
-  const [text, setText] = useState("");
 
-  const handleChange = (evt) => {
-    setText(evt.target.value);
-  }
-  const myName = "Frank";
-  const buttonText = "KLICKA INTE";
   return (
     <Router>
       <div>
@@ -29,16 +23,15 @@ function App() {
         <Switch>
           <Route path="/about">
             <p>About</p>
-            <MyComponent handleChange={handleChange} text={text} setText={setText} buttonText={buttonText} myName={myName} />
           </Route>
           <Route path="/portfolio">
-            <p>Portoflio</p>
+            <p>Portfolio</p>
           </Route>
           <Route path="/">
             <p>Home</p>
           </Route>
         </Switch>
-        <h1>Hello {text}</h1>
+        <h1>Hello</h1>
 
       </div>
     </Router>
@@ -46,23 +39,4 @@ function App() {
 }
 
 
-function MyComponent(props) {
-  //const { myName } = props;
-  console.log(props.myName)
-  return (
-    <>
-      <h2>Hej {props.myName} på riktigt från MyComponent</h2>
-      <MyButton {...props}>{props.buttonText}</MyButton>
-      <MyInput {...props} />
-    </>
-  )
-
-}
-
-function MyInput({ text, handleChange }) {
-
-
-
-  return (<input type="text" placeholder="Skriv" value={text} onChange={handleChange} />)
-}
 export default App;
